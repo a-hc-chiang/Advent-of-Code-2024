509d704f18a72ef8ff7725f3f5fcbfbf68197a08
Angela Chiang <achi2048@students.cs.ubc.ca>
2024-12-20 20:24:11 -0800

    day2 part2 (still incorrect answer TT)

--- advent_of_code_2024.py
+++ advent_of_code_2024.py
@@ -77,30 +77,47 @@ def day_2_part_2_solution():
 
     res = 0 
     for line in list: 
+        # print(line)
         i = 0
         curr_int = line[0]
-        is_increasing = False 
+        is_increasing = find_increasing(line)
         bad_val = 0
 
-        if curr_int > line[-1] or curr_int > line[-2]:
-            is_increasing = False
-        elif curr_int < line[-1] or curr_int < line[-2]:
-            is_increasing = True
-
         for j in range(1, len(line)): 
+            new_val = line[j]
+            # print("curr int: " + str(curr_int) + " new_val: " + str(new_val))
+            # print("i is: " + str(i))
             if is_increasing: 
                 if line[j] - curr_int > 3 or line[j] - curr_int <= 0:
                     bad_val += 1 
-                    if not check(line, is_increasing, j):  
-                        break 
+                    is_valid = None 
+                    if curr_int > line[j] and j == 1: 
+                        is_valid = check(line, is_increasing, 0)
+                    else: 
+                        is_valid = check(line, is_increasing, j)
+                    if is_valid:
+                        i += 1
+                        continue
+                    else:
+                        break
             else:
                 if line[j] - curr_int < -3 or line[j] - curr_int >= 0:
                     bad_val += 1 
-                    if not check(line, is_increasing, j): 
-                        break 
-            curr_int = line[j]
+                    is_valid = None 
+                    if curr_int < line[j] and j == 1: 
+                        is_valid = check(line, is_increasing, 0)
+                    else: 
+                        is_valid = check(line, is_increasing, j)
+                    if is_valid: 
+                        i += 1
+                        continue
+                    else: 
+                        break
+            curr_int = new_val
             i += 1
-
+        # print(str(i) + " i and bad_val is " + str(bad_val))
+        # print("i is: " + str(i) + " len is: " + str(len(line)))
+        # print(str(bad_val))
         if i == len(line) - 1 and bad_val < 2: 
             res += 1 
         else: 
@@ -108,6 +125,7 @@ def day_2_part_2_solution():
     print(str(res))
 
 def check(arr, is_increasing, remove_idx):
+    # print("remove idx: " + str(remove_idx))
     prev = None
     for i in range(len(arr)):
         if i == remove_idx:
@@ -119,6 +137,19 @@ def check(arr, is_increasing, remove_idx):
                 return False
         prev = arr[i]
     return True
+
+def find_increasing(line): 
+    # Calculate the trend based on differences between adjacent values
+    increasing, decreasing = 0, 0
+    for i in range(len(line) - 1):
+        diff = line[i + 1] - line[i]
+        # print("Diff: " + str(diff) + " increasing: " + str(increasing) + " decreasing: " + str(decreasing))
+        if diff > 0:
+            increasing += 1
+        elif diff < 0:
+            decreasing += 1
+    # print(increasing > decreasing)
+    return increasing > decreasing
 
 def main(): 
     #day_1_solution()
